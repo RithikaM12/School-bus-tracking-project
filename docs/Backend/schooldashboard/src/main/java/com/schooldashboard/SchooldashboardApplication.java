e5860fc5a78238833734b2939c5f00c2ac92b294
rittu <rithikakumar1209@gmail.com>
2026-08-08 22:43:25 +0530

    Backend setup

---
+++ docs/Backend/schooldashboard/src/main/java/com/schooldashboard/SchooldashboardApplication.java
@@ -0,0 +1,13 @@
+package com.schooldashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchooldashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchooldashboardApplication.class, args);
+	}
+
+}
---
+++ docs/Backend/schooldashboard/src/test/java/com/schooldashboard/SchooldashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schooldashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchooldashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
